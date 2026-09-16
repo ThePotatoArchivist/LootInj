4cb016447fff236caf64a8b326442d55b7ed5ead
ThePotatoArchivist <josiahfu@gmail.com>
2026-09-16 16:50:25 -0700

    Remove unused targets codec

--- src/main/java/archives/tater/lootinj/impl/LootInjImpl.java
+++ src/main/java/archives/tater/lootinj/impl/LootInjImpl.java
@@ -26,7 +26,6 @@ public class LootInjImpl implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(LootInj.MOD_ID);
 
     private static final MapCodec<List<ResourceKey<LootTable>>> TARGETS_MAP_CODEC = compactListCodec(ResourceKey.codec(Registries.LOOT_TABLE), ResourceKey.codec(Registries.LOOT_TABLE).listOf(1, Integer.MAX_VALUE)).fieldOf("targets");
-    public static final Codec<List<ResourceKey<LootTable>>> TARGETS_CODEC = TARGETS_MAP_CODEC.codec();
 
     public static final LootModificationTargetsManager targetsLoader = new LootModificationTargetsManager();
 
