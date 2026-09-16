2adc5078063c82ae2aaba785f135042fa20e09d6
ThePotatoArchivist <josiahfu@gmail.com>
2026-09-15 20:00:39 -0700

    Test reference

--- src/testmod/java/archives/tater/lootinj/test/LootInjTestDataGenerator.java
+++ src/testmod/java/archives/tater/lootinj/test/LootInjTestDataGenerator.java
@@ -3,7 +3,15 @@ package archives.tater.lootinj.test;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+
 public class LootInjTestDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void buildReloadableRegistry(RegistrySetBuilder registryBuilder) {
+        registryBuilder.add(Registries.PREDICATE, LootModificationProvider::bootstrap);
+    }
+
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         var pack = fabricDataGenerator.createPack();
--- src/testmod/java/archives/tater/lootinj/test/LootModificationProvider.java
+++ src/testmod/java/archives/tater/lootinj/test/LootModificationProvider.java
@@ -6,15 +6,24 @@ import archives.tater.lootinj.api.LootModification;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
+import static net.minecraft.advancements.predicates.ItemPredicate.Builder.item;
 import static net.minecraft.world.level.storage.loot.LootPool.lootPool;
 import static net.minecraft.world.level.storage.loot.entries.LootItem.lootTableItem;
+import static net.minecraft.world.level.storage.loot.predicates.MatchTool.toolMatches;
 
 public class LootModificationProvider extends FabricDynamicRegistryProvider {
     public LootModificationProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
@@ -23,14 +32,40 @@ public class LootModificationProvider extends FabricDynamicRegistryProvider {
 
     @Override
     protected void configure(HolderLookup.Provider registries, Entries entries) {
+        bootstrap(entriesContext(entries));
+
         entries.add(ResourceKey.create(LootInj.LOOT_MODIFICATION, LootInj.id("add_diamonds")), LootModification.builder()
-                .target(BuiltInLootTables.BASTION_BRIDGE)
+                .target(Blocks.DIRT.getLootTable().orElseThrow())
                 .pool(lootPool()
-                        .add(lootTableItem(Items.DIAMOND)))
-                .modifyPools(builder -> builder
-                        .add(lootTableItem(Items.STONE)))
+                        .add(lootTableItem(Items.DIAMOND))
+                        .when(registries.getOrThrow(A)))
                 .build()
         );
+    }
+
+    private static BootstrapContext<LootItemCondition> entriesContext(Entries entries) {
+        return new BootstrapContext<>() {
+            @Override
+            public Holder.Reference<LootItemCondition> register(ResourceKey<LootItemCondition> key, LootItemCondition value) {
+                return (Holder.Reference<LootItemCondition>) entries.add(key, value);
+            }
+
+            @Override
+            public <S> HolderGetter<S> lookup(ResourceKey<? extends Registry<? extends S>> key) {
+                return entries.getLookups().lookupOrThrow(key);
+            }
+
+            @Override
+            public <S> Stream<Holder.Reference<S>> listContextElements(ResourceKey<? extends Registry<? extends S>> key) {
+                return entries.getLookups().lookupOrThrow(key).listElements();
+            }
+        };
+    }
+
+    public static final ResourceKey<LootItemCondition> A = ResourceKey.create(Registries.PREDICATE, LootInj.id("a"));
+
+    public static void bootstrap(BootstrapContext<LootItemCondition> context) {
+        context.register(A, toolMatches(item().of(context.lookup(Registries.ITEM), Items.DIAMOND)).build());
     }
 
     @Override
