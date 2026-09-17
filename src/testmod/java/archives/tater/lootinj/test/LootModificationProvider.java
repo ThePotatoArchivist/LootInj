66ebf0c43339a263a5c06dadfabdd1bd0d2c7a87
ThePotatoArchivist <josiahfu@gmail.com>
2026-09-16 22:43:34 -0700

    Remove extra test patches

--- src/testmod/java/archives/tater/lootinj/test/LootModificationProvider.java
+++ src/testmod/java/archives/tater/lootinj/test/LootModificationProvider.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricCodecDataProvider;
 
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
@@ -15,12 +14,8 @@ import net.minecraft.world.level.block.Blocks;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
-import static net.minecraft.advancements.criterion.ItemPredicate.Builder.item;
 import static net.minecraft.world.level.storage.loot.LootPool.lootPool;
 import static net.minecraft.world.level.storage.loot.entries.LootItem.lootTableItem;
-import static net.minecraft.world.level.storage.loot.functions.SetItemCountFunction.setCount;
-import static net.minecraft.world.level.storage.loot.predicates.MatchTool.toolMatches;
-import static net.minecraft.world.level.storage.loot.providers.number.ConstantValue.exactly;
 
 public class LootModificationProvider extends FabricCodecDataProvider<LootModification> {
     protected LootModificationProvider(FabricPackOutput packOutput, CompletableFuture<HolderLookup.Provider> registriesFuture) {
@@ -29,16 +24,10 @@ public class LootModificationProvider extends FabricCodecDataProvider<LootModifi
 
     @Override
     protected void configure(BiConsumer<Identifier, LootModification> provider, HolderLookup.Provider registryLookup) {
-        var items = registryLookup.lookupOrThrow(Registries.ITEM);
-
         provider.accept(LootInj.id("add_diamonds"), LootModification.builder()
                 .target(Blocks.DIRT.getLootTable().orElseThrow())
                 .pool(lootPool()
                         .add(lootTableItem(Items.DIAMOND)))
-                .apply(setCount(exactly(1)))
-                .modifyPools(builder -> builder
-                        .apply(setCount(exactly(1)))
-                        .when(toolMatches(item().of(items, Items.DIAMOND))))
                 .build()
         );
     }
